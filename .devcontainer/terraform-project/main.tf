c3ab0bc1d2a67a60a8617ab5fe6b820f1e536774
QuadriBello <140855364+QuadriBello@users.noreply.github.com>
2023-12-08 23:40:31 +0000

    new save

---
+++ .devcontainer/terraform-project/main.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.30.0"
+    }
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+ profile = "terraform-user"
+}
+
+resource "aws_iam_user" "my-user" {
+  name = "quadri-user"
+  path = "/system/"
+}
